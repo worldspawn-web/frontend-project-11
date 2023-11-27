e3e0f707eee0ed54a90656bceb3b71b6fa658eca
Worldspawn <worldspawn.web@gmail.com>
2023-11-27 23:45:36 +0300

    prettier pls come back

--- src/app.js
+++ src/app.js
@@ -25,11 +25,12 @@ const addProxy = (url) => {
   return newUrl.toString();
 };
 
-const getData = (url) => axios.get(addProxy(url), {
-  headers: {
-    'Content-Type': 'application/xml',
-  },
-});
+const getData = (url) =>
+  axios.get(addProxy(url), {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
 
 // unique ids for every post
 const setIds = (posts, feedId) => {
@@ -48,28 +49,31 @@ const handleData = (data, watchedState) => {
 };
 
 const updatePosts = (watchedState) => {
-  const promises = watchedState.feeds.map((feed) => getData(feed.link)
-    .then((response) => {
-      const { posts } = parse(response.data.contents);
-      const statePosts = watchedState.posts;
-      const currentIdPosts = statePosts.filter(
-        (post) => post.feedId === feed.id,
-      );
-      const displayedPostLinks = currentIdPosts.map((post) => post.link);
-      const newPosts = posts.filter(
-        (post) => !displayedPostLinks.includes(post.link),
-      );
-      setIds(newPosts, feed.id);
-      watchedState.posts.unshift(...newPosts);
-    })
-    .catch((error) => {
-      console.error(
-        `Whoops, something is wrong with fetching data from feed ${feed.id}:`,
-        error,
-      );
-    }),
+  const promises = watchedState.feeds.map((feed) =>
+    getData(feed.link)
+      .then((response) => {
+        const { posts } = parse(response.data.contents);
+        const statePosts = watchedState.posts;
+        const currentIdPosts = statePosts.filter(
+          (post) => post.feedId === feed.id,
+        );
+        const displayedPostLinks = currentIdPosts.map((post) => post.link);
+        const newPosts = posts.filter(
+          (post) => !displayedPostLinks.includes(post.link),
+        );
+        setIds(newPosts, feed.id);
+        watchedState.posts.unshift(...newPosts);
+      })
+      .catch((error) => {
+        console.error(
+          `Whoops, something is wrong with fetching data from feed ${feed.id}:`,
+          error,
+        );
+      }),
   );
-  return Promise.all(promises).finally(() => setTimeout(() => updatePosts(watchedState), 5000));
+  return Promise.all(promises).finally(() =>
+    setTimeout(() => updatePosts(watchedState), 5000),
+  );
 };
 
 const handleError = (error) => {
